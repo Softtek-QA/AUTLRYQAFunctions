a059c92bafc54fe44142f387383331de82562af5
marcosoliveirastk <marcos.oliveira@softtek.com>
2018-09-15 10:08:41 -0300

    SPRINT 1 - V1

--- AUTVAProjectFunctions.java
+++ AUTVAProjectFunctions.java
@@ -3,7 +3,10 @@
 import com.borland.silktest.jtf.BrowserBaseState;
 import com.borland.silktest.jtf.Desktop;
 import com.borland.silktest.jtf.common.BrowserType;
+import com.borland.silktest.jtf.win32.AccessibleControl;
+import com.borland.silktest.jtf.xbrowser.BrowserApplication;
 import com.borland.silktest.jtf.xbrowser.DomButton;
+import com.borland.silktest.jtf.xbrowser.DomElement;
 import com.borland.silktest.jtf.xbrowser.DomTextField;
 import br.lry.functions.*;
 import br.lry.functions.AUTProjectsFunctions.AUTLogMensagem;
@@ -35,18 +38,53 @@ public abstract class AUTVAProjectFunctions {
 		try {
 			
 			AUT_CURRENT_LOG_OBJECT.logMensagem("AUT INFO: INICIANDO LOGIN : APLICACAO VA");
+			if(agent.<BrowserApplication>find("VA02").exists("Mensagem",10000)) {
+				agent.<AccessibleControl>find("VA02.Mensagem").click();
+			}
+			agent.<DomElement>find("VA.TelaLoginHomog1.EntrarMenuInicial").click();
+			agent.<DomTextField>find("VA.TelaLoginHomog1.Usuario").click();
+			agent.<DomTextField>find("VA.TelaLoginHomog1.Usuario").setText(user);
+			agent.<DomTextField>find("VA.TelaLoginHomog1.Usuario").click();
+			agent.<DomTextField>find("VA.TelaLoginHomog1.Senha").setText(password);
+			agent.<DomButton>find("VA.TelaLoginHomog1.EntrarLogin").click();
 			
-			agent.<DomTextField>find("VA.Login.Usuario").setText(user);
-			agent.<DomTextField>find("VA.Login.Senha").setText(password);
-			agent.<DomButton>find("VA.Login.Avancar").click();
-
 			AUT_CURRENT_LOG_OBJECT.logMensagem("AUT INFO: LOGIN REALIZADO COM SUCESSO");
 			return true;
 		} catch (java.lang.Exception e) {
-
+			
 			AUT_CURRENT_LOG_OBJECT.logMensagem(AUT_TIPO_MSG_LOG.MENSAGEM_INFORMATIVA,
 					"AUT ERROR: LOGIN : APLICACAO VA");
 
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			
+			return false;
+		}
+	}
+	
+	
+	
+	public static boolean autLoginVA(Desktop agent, String user, String password) {
+		try {
+			
+			AUT_CURRENT_LOG_OBJECT.logMensagem("AUT INFO: INICIANDO LOGIN : APLICACAO VA");
+			
+			agent.<DomTextField>find("VA.Login.Usuario").click();
+			agent.<DomTextField>find("VA.Login.Usuario").setText(user);
+			agent.<DomTextField>find("VA.Login.Senha").click();
+			agent.<DomTextField>find("VA.Login.Senha").setText(password);
+			agent.<DomButton>find("VA.Login.Avancar").click();
+			
+			AUT_CURRENT_LOG_OBJECT.logMensagem("AUT INFO: LOGIN REALIZADO COM SUCESSO");
+			return true;
+		} catch (java.lang.Exception e) {
+			
+			AUT_CURRENT_LOG_OBJECT.logMensagem(AUT_TIPO_MSG_LOG.MENSAGEM_INFORMATIVA,
+					"AUT ERROR: LOGIN : APLICACAO VA");
+
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			
 			return false;
 		}
 	}
